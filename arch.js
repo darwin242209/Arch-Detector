77b6fb7e1497984471b387663acb939f454b2272
Darwin <115344849+darwin242209@users.noreply.github.com>
2024-07-06 22:02:08 +0800

    Create arch.js

---
+++ arch.js
@@ -0,0 +1,7 @@
+const os = require('os');
+
+// Get the architecture of the machine
+const architecture = os.arch();
+
+// Print out the architecture
+console.log(`The architecture of this machine is: ${architecture}`);
