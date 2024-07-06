39b7708726a13b2b5eb668466ff58c63af098760
Darwin <115344849+darwin242209@users.noreply.github.com>
2024-07-06 22:03:09 +0800

    Update arch.js

--- arch.js
+++ arch.js
@@ -1,7 +1,18 @@
+const express = require('express');
 const os = require('os');
+
+const app = express();
+const port = 3000;
 
 // Get the architecture of the machine
 const architecture = os.arch();
 
-// Print out the architecture
-console.log(`The architecture of this machine is: ${architecture}`);
+// Define a route to display the architecture
+app.get('/', (req, res) => {
+    res.send(`<h1>The architecture of this machine is: ${architecture}</h1>`);
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
